b0aa96e5683ff2d213e2832a7be17efc2bc897e2
Tobias <aichinger.tobias13@gmail.com>
2022-05-03 06:34:11 +0200

    Removed create Board from main and added board.rs to handle board actions

---
+++ chess/src/board.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+
+const BROWN_COLOR: Color = Color::rgb(181.0 / 255.0, 136.0 / 255.0, 99.0 / 255.0);
+const LIGTH_BROWN_COLOR: Color = Color::rgb(240.0 / 255.0, 217.0 / 255.0, 181.0 / 255.0);
+
+pub struct Board;
+pub struct BoardPlugin;
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(create_board);
+    }
+}
+
+#[derive(Component)]
+pub struct Square;
+
+fn create_board(mut commands: Commands) {
+    // Cameras
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+
+
+    // Create Chessboard 8x8
+    for row in 0..8 {
+        for column in 0..8 {
+            let square_position = Vec2::new(
+                super::OFFSET + column as f32 * (super::SQUARE_SIZE),
+                super::OFFSET + row as f32 * (super::SQUARE_SIZE),
+            );
+
+            if (row + column) % 2 != 0 {
+                // Insert brown square
+                commands
+                .spawn()
+                .insert(Square)
+                .insert_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        color: LIGTH_BROWN_COLOR,
+                        ..default()
+                    },
+                    transform: Transform {
+                        translation: square_position.extend(0.0),
+                        scale: Vec3::new(super::SQUARE_SIZE, super::SQUARE_SIZE, 0.0),
+                        ..default()
+                    },
+                    ..default()
+                });
+            }
+            else {
+                // Insert white square 
+                commands
+                .spawn()
+                .insert(Square)
+                .insert_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        color: BROWN_COLOR,
+                        ..default()
+                    },
+                    transform: Transform {
+                        translation: square_position.extend(0.0),
+                        scale: Vec3::new(super::SQUARE_SIZE, super::SQUARE_SIZE, 1.0),
+                        ..default()
+                    },
+                    ..default()
+                });
+            }
+        }
+    }
+}
--- chess/src/main.rs
+++ chess/src/main.rs
@@ -5,16 +5,16 @@ Bevy version:   "0.7" / 2022
 
 use bevy::{prelude::*, window::WindowResizeConstraints};
 
-mod piece;
-
-use piece::*;
-
 // Calculate offset because piece is always placed in the middle
 const OFFSET: f32 = (-(8 as f32 / 2.0 * SQUARE_SIZE)) + SQUARE_SIZE / 2.;
 
 const SQUARE_SIZE: f32 = 75.0;
-const BROWN_COLOR: Color = Color::rgb(181.0 / 255.0, 136.0 / 255.0, 99.0 / 255.0);
-const LIGTH_BROWN_COLOR: Color = Color::rgb(240.0 / 255.0, 217.0 / 255.0, 181.0 / 255.0);
+
+mod piece;
+mod board;
+
+use piece::*;
+use board::*;
 
 fn main() {
     App::new()
@@ -32,66 +32,6 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(PiecePlugin)
-        .add_startup_system(create_board)
+        .add_plugin(BoardPlugin)
         .run();
-}
-
-#[derive(Component)]
-struct Square;
-
-#[derive(Component)]
-struct King;
-
-fn create_board(mut commands: Commands) {
-    // Cameras
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(UiCameraBundle::default());
-
-
-    // Create Chessboard 8x8
-    for row in 0..8 {
-        for column in 0..8 {
-            let square_position = Vec2::new(
-                OFFSET + column as f32 * (SQUARE_SIZE),
-                OFFSET + row as f32 * (SQUARE_SIZE),
-            );
-
-            if (row + column) % 2 != 0 {
-                // Insert brown square
-                commands
-                .spawn()
-                .insert(Square)
-                .insert_bundle(SpriteBundle {
-                    sprite: Sprite {
-                        color: LIGTH_BROWN_COLOR,
-                        ..default()
-                    },
-                    transform: Transform {
-                        translation: square_position.extend(0.0),
-                        scale: Vec3::new(SQUARE_SIZE, SQUARE_SIZE, 0.0),
-                        ..default()
-                    },
-                    ..default()
-                });
-            }
-            else {
-                // Insert white square 
-                commands
-                .spawn()
-                .insert(Square)
-                .insert_bundle(SpriteBundle {
-                    sprite: Sprite {
-                        color: BROWN_COLOR,
-                        ..default()
-                    },
-                    transform: Transform {
-                        translation: square_position.extend(0.0),
-                        scale: Vec3::new(SQUARE_SIZE, SQUARE_SIZE, 1.0),
-                        ..default()
-                    },
-                    ..default()
-                });
-            }
-        }
-    }
 }
