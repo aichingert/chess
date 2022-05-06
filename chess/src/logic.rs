c957c8bfeaea8ae2304d5e1699a267233f03cf0b
Tobias <aichinger.tobias13@gmail.com>
2022-05-06 10:04:55 +0200

    Added Logic file for game checking possible moves

---
+++ chess/src/logic.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+mod piece;
+
+use piece::*;
+
+impl Piece {
+    fn check_possible_moves(self, pieces: Vec<Piece>) {
+        for p in pieces {
+            
+        }
+    }
+}
