31c1b932662da0a6d16d8d9ef5368967682d5191
Tobias <aichinger.tobias13@gmail.com>
2022-09-22 10:42:29 +0200

    added rook tests

--- chess/src/tests.rs
+++ chess/src/tests.rs
@@ -11,30 +11,45 @@ mod test {
     }
 
     #[test]
-    fn pawn_no_possible_move_when_blocked() {
-        let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (0, 1)), Piece::black(Kind::Pawn, (0, 2))];
+    fn pawn_unable_to_move_when_blocked() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Pawn, (0, 1)), 
+            Piece::black(Kind::Pawn, (0, 2))
+        ];
 
         assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![]);
     }
 
     #[test]
     fn pawn_able_to_take_while_blocked() {
-        let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (0, 1)), Piece::black(Kind::Pawn, (0, 2)), Piece::black(Kind::Pawn, (1, 2))];
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Pawn, (0, 1)), 
+            Piece::black(Kind::Pawn, (0, 2)), 
+            Piece::black(Kind::Pawn, (1, 2))
+        ];
 
         assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![(1,2)]);
     }
 
     #[test]
     fn pawn_en_passnt_check() {
-        let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (1, 4)), Piece::black(Kind::Pawn, (0, 4))];
-        let moves: Vec<Move> = vec![Move::new(Piece::black(Kind::Pawn, (0, 4)), (0, 6), (0, 4))];
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Pawn, (1, 4)), 
+            Piece::black(Kind::Pawn, (0, 4))
+        ];
+        let moves: Vec<Move> = vec![
+            Move::new(Piece::black(Kind::Pawn, (0, 4)), (0, 6), (0, 4))
+        ];
 
         assert!(pieces[0].get_moves(&pieces, &moves, false) == vec![(0, 5), (1, 5)]);
     }
 
     #[test]
     fn pawn_en_passant_not_possible_check() {
-        let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (1, 4)), Piece::black(Kind::Pawn, (0, 4))];
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Pawn, (1, 4)), 
+            Piece::black(Kind::Pawn, (0, 4))
+        ];
         let moves: Vec<Move> = vec![];
 
         assert!(pieces[0].get_moves(&pieces, &moves, false) == vec![(1, 5)]);
@@ -42,7 +57,10 @@ mod test {
 
     #[test]
     fn pawn_take_another_piece() {
-        let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (1, 4)), Piece::black(Kind::Pawn, (0, 5))];
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Pawn, (1, 4)), 
+            Piece::black(Kind::Pawn, (0, 5))
+        ];
 
         assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![(0, 5), (1, 5)]);
     }
@@ -53,7 +71,7 @@ mod test {
 
         let pieces: Vec<Piece> = vec![Piece::white(Kind::Pawn, (1, 7))];
 
-        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![]);
+        assert!(false);
     }
 
     #[test]
@@ -65,5 +83,42 @@ mod test {
             (2, 3), (1, 3), (0, 3),
             (3, 4), (3, 5), (3, 6), (3, 7), 
             (3, 2), (3, 1), (3, 0)]);
+    }
+
+    #[test]
+    fn rook_corner_moves() {
+        let pieces: Vec<Piece> = vec![Piece::white(Kind::Rook, (0, 0))];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
+            (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+            (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)
+        ]);
+    }
+
+    #[test]
+    fn rook_unable_to_move_when_blocked() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Rook, (3, 3)),
+            Piece::white(Kind::Pawn, (4, 3)),
+            Piece::white(Kind::Pawn, (2, 3)),
+            Piece::white(Kind::Pawn, (3, 2)),
+            Piece::white(Kind::Pawn, (3, 4))
+        ];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![]);
+    }
+
+    #[test]
+    fn rook_one_side_blocked() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Rook, (2, 2)),
+            Piece::white(Kind::Pawn, (2, 3))
+        ];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
+            (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), 
+            (1, 2), (0, 2), 
+            (2, 1), (2, 0)
+        ]);
     }
 }
