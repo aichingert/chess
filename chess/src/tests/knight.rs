2eaa920d29fa58ea1f7b3f2b064a591e65bf6a1d
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-09-26 22:25:50 +0200

    tests (stable): added knights bounds test 'knight_on_right_side'

--- chess/src/tests/knight.rs
+++ chess/src/tests/knight.rs
@@ -13,4 +13,18 @@ mod test {
             (1, 6), (1, 2)
         ]);
     }
+
+    #[test]
+    fn knight_on_right_side() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Knight, (7, 4))
+        ];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
+            (5, 5), (5, 3),
+            (6, 6), (6, 2)
+        ]);
+    }
+
+
 }
