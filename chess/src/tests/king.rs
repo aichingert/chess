8ae1762cb43754d073a26ffd4c4aec74bfae100b
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-09-27 21:41:50 +0200

    test: added 'king_in_the_middle' test

--- chess/src/tests/king.rs
+++ chess/src/tests/king.rs
@@ -12,4 +12,17 @@ mod test {
             (0, 1), (1, 0), (1, 1)
         ]);
     }
+
+    #[test]
+    fn king_in_the_middle() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::King, (3, 3))
+        ];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
+            (2, 2), (2, 3), (2, 4),
+            (3, 2)        , (3, 4),
+            (4, 2), (4, 3), (4, 4)
+        ]);
+    }
 }
