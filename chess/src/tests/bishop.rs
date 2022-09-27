d485db30700942fe52f8792c060e4c3ce6efb1da
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-09-27 21:24:48 +0200

    test: added bishop 'middle_of_the_board' test

---
+++ chess/src/tests/bishop.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod test {
+    use crate::piece::*;
+
+    #[test]
+    fn bishop_middle_of_the_board() {
+        let pieces: Vec<Piece> = vec![
+            Piece::white(Kind::Bishop, (3, 3))
+        ];
+
+        assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
+            (4, 4), (5, 5), (6, 6), (7, 7),
+            (2, 2), (1, 1), (0, 0), 
+            (2, 4), (1, 5), (0, 6),
+            (1, 2), (2, 1)
+        ]);
+    }
+}
