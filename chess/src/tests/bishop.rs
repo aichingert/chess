0d1979766f3c3477923a2e5ad8dfd35a401a36fb
Tobias <aichinger.tobias13@gmail.com>
2022-09-29 11:58:22 +0200

    test (fix): corrected assert statement

--- chess/src/tests/bishop.rs
+++ chess/src/tests/bishop.rs
@@ -8,12 +8,11 @@ mod test {
             Piece::white(Kind::Bishop, (3, 3))
         ];
 
-        println!("{:?}", pieces[0].get_moves(&pieces, &vec![], false));
         assert!(pieces[0].get_moves(&pieces, &vec![], false) == vec![
             (4, 4), (5, 5), (6, 6), (7, 7),
-            (2, 2), (1, 1), (0, 0), 
+            (4, 2), (5, 1), (6, 0),
             (2, 4), (1, 5), (0, 6),
-            (1, 2), (2, 1)
+            (2, 2), (1, 1), (0, 0), 
         ]);
     }
 }
